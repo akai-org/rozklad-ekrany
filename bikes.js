9462c6ab33ce2d3d59dbb6377306dbc0b1bacb90
Marcin Lawniczak <marcin@lawniczak.me>
2018-03-20 20:30:44 +0100

    Add bike status from nextbike API

---
+++ bikes.js
@@ -0,0 +1,26 @@
+const apiUrl = 'https://api.nextbike.net/maps/nextbike-live.json?city=192'
+//ID stacji
+const POLITECHNIKA = 6167
+const KORNICKA = 2189
+
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+        var status = xhr.status;
+        if (status === 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status, xhr.response);
+        }
+    };
+    xhr.send();
+};
+
+var getBikeStation = function(data, uid) {
+    return data.countries[0].cities[0].places.filter( function (value) {
+        return value.number === uid
+    })[0]
+}
+
