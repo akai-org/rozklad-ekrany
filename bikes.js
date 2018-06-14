6888043ce2c2a757c92446e6e58b154fad070272
Marcin Jachymski <marcinjachymski@gmail.com>
2018-06-14 21:09:31 +0200

    Add a test to the bikes response (Austria is now the first country, apparently)

--- bikes.js
+++ bikes.js
@@ -1,12 +1,25 @@
 //Poznań ma ID 192, wysyłamy żeby nie pobierać wszystkich rowerów nextbike, a trochę ich mają ;)
-const nextbikeApiUrl = 'https://api.nextbike.net/maps/nextbike-live.json?city=192'
+const nextbikeApiUrl = 'https://api.nextbike.net/maps/nextbike-live.json?city=192';
 //ID stacji
-const POLITECHNIKA = 6167
-const KORNICKA = 2189
+const POLITECHNIKA = 6167;
+const KORNICKA = 2189;
 
 var getBikeStation = function(data, uid) {
-    return data.countries[0].cities[0].places.filter( function (value) {
-        return value.number === uid
-    })[0]
-}
+    var country_id = 0;
+    try {
+        return data.countries[country_id].cities[0].places.filter( function (value) {
+            return value.number === uid
+        })[0]
+    } catch(e) {
+        console.log(e.name + ': ' + e.message);
+        if(e instanceof TypeError) {
+            while(data.countries[country_id].name !== "PRM Poznan Poland") {
+                country_id++;
+            }
+        }
+        return data.countries[country_id].cities[0].places.filter( function (value) {
+            return value.number === uid
+        })[0]
+    }
+};
 
