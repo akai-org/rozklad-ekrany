64f9cfe0b9c6e7f661f12b1a3e73a35de5af5e20
Marcin Jachymski <marcinjachymski@gmail.com>
2018-06-16 20:41:02 +0200

    Slim down the getBikeStation function

--- bikes.js
+++ bikes.js
@@ -4,22 +4,13 @@ const nextbikeApiUrl = 'https://api.nextbike.net/maps/nextbike-live.json?city=19
 const POLITECHNIKA = 6167;
 const KORNICKA = 2189;
 
-var getBikeStation = function(data, uid) {
-    var country_id = 0;
-    try {
-        return data.countries[country_id].cities[0].places.filter( function (value) {
-            return value.number === uid
-        })[0]
-    } catch(e) {
-        console.log(e.name + ': ' + e.message);
-        if(e instanceof TypeError) {
-            while(data.countries[country_id].name !== "PRM Poznan Poland") {
-                country_id++;
-            }
-        }
-        return data.countries[country_id].cities[0].places.filter( function (value) {
-            return value.number === uid
-        })[0]
+const getBikeStation = function(data, uid) {
+    let country_id = 0;
+    while(data.countries[country_id].name !== "PRM Poznan Poland") {
+        country_id++;
     }
+    return data.countries[country_id].cities[0].places.filter( function (value) {
+        return value.number === uid;
+    })[0];
 };
 
