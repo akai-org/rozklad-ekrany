0807f49976fc56815de1294e923ab2c4051703e4
Marcin Jachymski <marcinjachymski@gmail.com>
2018-08-12 19:39:14 +0200

    Add dynamic table generation, new styling

--- bikes.js
+++ bikes.js
@@ -1,16 +1,11 @@
 //Poznań ma ID 192, wysyłamy żeby nie pobierać wszystkich rowerów nextbike, a trochę ich mają ;)
 const nextbikeApiUrl = 'https://api.nextbike.net/maps/nextbike-live.json?city=192';
-//ID stacji
-const POLITECHNIKA = 6167;
-const KORNICKA = 2189;
 
-const getBikeStation = function(data, uid) {
-    let country_id = 0;
-    while(data.countries[country_id].name !== "PRM Poznan Poland") {
-        country_id++;
-    }
-    return data.countries[country_id].cities[0].places.filter( function (value) {
-        return value.number === uid;
-    })[0];
+const getBikeStation = (data, stopName) => {
+    return data
+        .countries.filter(country => country.name === "PRM Poznan Poland")[0]
+        .cities.filter(city => city.name === "Poznań")[0]
+        .places.filter(place => place.name === stopName)[0]
+        .bikes;
+
 };
-
--- trams.js
+++ trams.js
@@ -1,33 +1,56 @@
 //Adres API
-const vmApiUrl = 'https://rozklad.akai.org.pl/'
+const vmApiUrl = 'https://rozklad.akai.org.pl/';
 //https://www.peka.poznan.pl/vm/
 
 //Przystanki
 //www.peka.poznan.pl/vm/?przystanek=PP72
-//Politechnika w kierunku miasta
-const POLITECHNIKA_WEST = 'PP72';
-//Politechnika w kierunku na Franowo/Starołękę
-const POLITECHNIKA_EAST = 'PP71';
 
-//Kórnicka na północ
-const KORNICKA_NORTH = 'KORN41';
-//Kórnicka na południe -
-const KORNICKA_SOUTH = 'KORN42';
-//Kórnicka na wschód
-const KORNICKA_EAST = 'KORN43';
-//Kónicka na zachód
-const KORNICKA_WEST = 'KORN44';
-//Kórnicka na południe 2 (pojedynczy przystanek)
-const KORNICKA_SOUTH_2 = 'KORN45';
+const views = [
+    [{symbol: 'PP71', desc: "W stronę Rataj"},              //Politechnika w kierunku na Franowo/Starołękę
+        {symbol: 'PP72', desc: 'W stronę Centrum'}],        //Politechnika w kierunku miasta
+    [{symbol: 'BAKA41', desc: 'W stronę Ronda Rataje'},     //Baraniaka na północ - Wilczak, Ogrody
+        {symbol: 'BAKA42', desc: 'W stronę Ronda Śródka'}], //Baraniaka na południe - Franowo, Starołęka
+    [{symbol: 'KORN41', desc: 'W stronę Ronda Rataje'},     //Kórnicka na północ
+        {symbol: 'KORN42', desc: 'W stronę Malty'}],        //Kórnicka na południe
+    [{symbol: 'KORN43', desc: 'W stronę Franowa'},          //Kórnicka na wschód
+        {symbol: 'KORN44', desc: 'W stronę Centrum'}],      //Kónicka na zachód
+    [{symbol: 'KORN45', desc: 'W stronę Malty'}]            //Kórnicka na południe 2 (pojedynczy przystanek)
+];
 
+const buildTable = function(model){
+    const table = document.createElement("table");
+    const top_row = document.createElement("tr");
+    const tableHeaders = {'line' : 'Linia', 'direction': 'Kierunek', 'departure' : 'Odjazd'};
 
-//Baraniaka na północ - Wilczak, Ogrody
-const BARANIAKA_NORTH = 'BAKA41';
-//Baraniaka na południe - Franowo, Starołęka
-const BARANIAKA_SOUTH = 'BAKA42';
+    Object.keys(tableHeaders).map(key => {
+        const el = document.createElement('th');
+        el.class = key;
+        el.textContent = tableHeaders[key];
+        top_row.appendChild(el);
+    });
+    table.appendChild(top_row);
 
-var views = [[POLITECHNIKA_WEST, POLITECHNIKA_EAST], [BARANIAKA_NORTH, BARANIAKA_SOUTH], [KORNICKA_NORTH, KORNICKA_SOUTH],
-    [KORNICKA_EAST, KORNICKA_WEST], KORNICKA_SOUTH_2];
+    model.times.slice(0,7).forEach(function(item) {
+        const new_row = document.createElement("tr");
+
+        ["line", "direction", "departure"].forEach(property => {
+            const el = document.createElement("td");
+            el.className = property;
+            let text = item[property];
+
+            if(property === "departure") {
+                text = text.slice(11,16);
+            }
+
+            const textNode = document.createTextNode(text);
+            el.appendChild(textNode);
+            new_row.appendChild(el);
+        });
+        table.appendChild(new_row);
+    });
+
+    return table;
+};
 
 var VM = {
     ver: 1.0
